164d01a2fda794b87f03eab31746de86dcc27cbd
Erik Lundin <c.erik.lundin@gmail.com>
2020-10-09 22:06:34 +0200

    one more

--- rostbot/index.js
+++ rostbot/index.js
@@ -87,6 +87,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
     console.log(`\n${ server.name } listening to ${ server.url }`);
     console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+    console.log(new Date());
 });
 
 // Listen for incoming activities and route them to your bot main dialog.
