bf9e001ff3b949aa4edbcf5bc5eea08ad50297f6
Erik Lundin <c.erik.lundin@gmail.com>
2020-10-11 14:53:52 +0200

    overridden initializeroutes function

--- dline/index.js
+++ dline/index.js
@@ -2,15 +2,16 @@ const directline = require('offline-directline');
 const express = require('express');
 
 const app = express();
-//app.listen(4000, '0.0.0.0'); //Make server listen on 0.0.0.0
-directline.initializeRoutes(app, 'http://0.0.0.0:4000', 'http://0.0.0.0:8080', false);
+directline.initializeRoutes = function(app, botUrl, conversationInitRequired = true) {
+    directLineEndpoint = 'http://0.0.0.0:3000';
+    const router = directline.getRouter(directLineEndpoint, botUrl, conversationInitRequired);
+    app.use(router);
+    app.listen(3000, () => {
+        console.log("Custom init...");
+        console.log('Listening for messages on ' + directLineEndpoint + '/directline');
+        console.log('Routing messages to bot on ' + botUrl);
+        
+    });
+}
 
-app.get("/",function(request, response){
-    var id = request.params.id;
-
-    response.writeHead(200, {"Content-Type": "application/json"});
-    response.write(JSON.stringify('Hello'));
-    console.log("someone accessed our site");
-});
-
-console.log("directline rerouter created, listening on http://0.0.0.0:4000");
+directline.initializeRoutes(app, 'http://0.0.0.0:8080', true);
