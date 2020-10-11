f6952d19ce2a97316e0d9598172ab50258efd7cf
Erik Lundin <c.erik.lundin@gmail.com>
2020-10-11 12:26:41 +0200

    make rerouter listen on 0.0.0.0

--- dline/index.js
+++ dline/index.js
@@ -2,7 +2,8 @@ const directline = require('offline-directline');
 const express = require('express');
 
 const app = express();
-directline.initializeRoutes(app, 3000, 'http://0.0.0.0:8080');
+app.listen(3000, '0.0.0.0'); //Make server listen on 0.0.0.0
+directline.initializeRoutes(app, 3000, 'http://0.0.0.0:8080', false);
 
 app.get("/",function(request, response){
     var id = request.params.id;
@@ -11,3 +12,5 @@ app.get("/",function(request, response){
     response.write(JSON.stringify('Hello'));
     console.log("someone accessed our site");
 });
+
+console.log("directline rerouter created, listening on http://0.0.0.0:3000");
