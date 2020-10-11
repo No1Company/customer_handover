a43d389aaaf50bf5a66b99c1fba9012ecf671f00
Erik Lundin <c.erik.lundin@gmail.com>
2020-10-11 11:21:38 +0200

    app name update

--- dline/index.js
+++ dline/index.js
@@ -3,3 +3,11 @@ const express = require('express');
 
 const app = express();
 directline.initializeRoutes(app, 3000, 'http://0.0.0.0:8080');
+
+app.get("/",function(request, response){
+    var id = request.params.id;
+
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.write(JSON.stringify('Hello'));
+    console.log("someone accessed our site");
+});
