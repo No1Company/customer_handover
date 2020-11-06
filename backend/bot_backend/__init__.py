0d1875c35acdc3ce0e73c10832985d87482abea5
Isak Almquist <28620471+isakalmq@users.noreply.github.com>
2020-11-06 14:24:56 +0100

    Add route for times

--- backend/bot_backend/__init__.py
+++ backend/bot_backend/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__, static_folder = 'static', static_url_path = '/')
 
-@app.route("/")
+@app.route('/')
 def main():
-    return "Hello world"
+    return 'Hello world'
+
+@app.route('/available-times', methods=['GET'])
+def avail_times():
+    times = [datetime.datetime(2020, 11, 7, 14, 30, 0, tzinfo=datetime.timezone.)]
+    return ""
