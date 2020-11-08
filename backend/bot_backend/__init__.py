40b510c1ccb299289ef3242d6c5ae9c1cb29d21d
Isak Almquist <isaal360@student.liu.se>
2020-11-08 14:43:04 +0100

    Make route actually provide times

--- backend/bot_backend/__init__.py
+++ backend/bot_backend/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, jsonify
+from datetime import datetime as d
 
 app = Flask(__name__, static_folder = 'static', static_url_path = '/')
 
@@ -9,5 +9,32 @@ def main():
 
 @app.route('/available-times', methods=['GET'])
 def avail_times():
-    times = [datetime.datetime(2020, 11, 7, 14, 30, 0, tzinfo=datetime.timezone.)]
-    return ""
+    times = [
+        {
+            "start" : d(2020, 11, 7, 14, 30, 0),
+            "stop"  : d(2020, 11, 7, 15, 30, 0)
+        },
+
+        {
+            "start" : d(2020, 11, 8, 11, 0, 0),
+            "stop"  : d(2020, 11, 8, 11, 30, 0)
+        },
+        
+        {
+            "start" : d(2020, 11, 8, 14, 30, 0),
+            "stop"  : d(2020, 11, 8, 15, 30, 0)
+        },
+        
+        {
+            "start" : d(2020, 11, 9, 8, 15, 0),
+            "stop"  : d(2020, 11, 9, 8, 45, 0)
+        },
+        
+        {
+            "start" : d(2020, 11, 9, 10, 30, 0),
+            "stop"  : d(2020, 11, 9, 11, 00, 0)
+        }
+        ]
+
+
+    return jsonify([ {"start": time["start"].isoformat(), "stop" : time["stop"].isoformat()} for time in times ])
