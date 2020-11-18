deda7a8e08eb728a5bacde6aed35f4538b73f8b6
Isak Almquist <isaal360@student.liu.se>
2020-11-18 18:13:16 +0100

    Repair things

--- backend/resetDB.py
+++ backend/resetDB.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-<<<<<<< HEAD
 from bot_backend.databasemgmt import *
 resetDB()
 createSmallExample()
-=======
-from bot_backend.databasemgmt import resetDB
-resetDB()
->>>>>>> origin/develop
