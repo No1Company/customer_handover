1a5ac0aa1f8031ea03569f091a99e66eb9b2a42f
Erik Lundin <erilu186@student.liu.se>
2020-12-06 16:17:49 +0100

    swap script

--- rostbot/dialogs/swap.py
+++ rostbot/dialogs/swap.py
@@ -7,13 +7,26 @@ for subdir, dirs, files in os.walk(rootdir):
         if any([s in file for s in [".en-us.lg", ".sv.lg"]]):
             path = os.path.join(subdir, file)
 
-            with open(path, "r+") as f:
-                for line in f:
+            values = []
+            with open(path, "rt") as fr:
+               
+                for line in fr:
                     if '"data":' in line:
                         value = line.replace('"data": ', '')
                         value = value.replace("  ", "")
                         value = value.replace("\n", "")
-                        print("'" + value + "'")
+                        value = value.replace(",", "")
+                        if "{" not in value:
+                            print("ersätt")
+                            print(value)
+                            values.append(value)
+                            
+                text = fr.read()
+                for value in values:
+                    text.replace(value, '{\n"msteams": {\n"type": "imBack",\n"value": '+value+'\n}\n}')
+                fr.close()
 
-
+            with open(path, "wt") as fw:
+                fw.write(text)
+                fw.close()
 print("end")
